dc914a43f253dc82156c7079e9b232e0511fa3c1
RA341 <razerishi1505@gmail.com>
2024-06-16 21:24:28 -0400

    fix: added condition for development

--- app/app.py
+++ app/app.py
@@ -1,3 +1,5 @@
+import os
+
 from flask import Flask, jsonify, request
 from waitress import serve
 
@@ -69,5 +71,5 @@ def main():
     serve(app, host="0.0.0.0", port=port)
 
 
-if __name__ == '__main__':
+if os.getenv('dev') is not None:
     main()
